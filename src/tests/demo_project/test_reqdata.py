fb4e4db32ed0907de6ec67af9029d00b03d872bf
Itai Shirav <itais@infinidat.com>
2018-02-05 17:54:21 +0200

    handle unicode request data

--- src/tests/demo_project/test_reqdata.py
+++ src/tests/demo_project/test_reqdata.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from test_base import BaseTestCase
 from testfixtures import LogCapture
 import json
@@ -27,11 +29,18 @@ class ReqDataTestCase(BaseTestCase):
                 ('reqdata', 'INFO', '(Anonymous) POST /admin/auth/group/add/\n    {\n        "name": "aaa"\n    }')
             )
 
+    def test_json_unicode(self):
+        with LogCapture('reqdata') as l:
+            self.client.post('/admin/auth/group/add/', json.dumps({'name': u'דוד'}), content_type='application/json')
+            l.check(
+                ('reqdata', 'INFO', u'(Anonymous) POST /admin/auth/group/add/\n    {\n        "name": "דוד"\n    }')
+            )
+
     def test_invalid_json(self):
         with LogCapture('reqdata') as l:
-            self.client.post('/admin/auth/group/add/', '{' + json.dumps({'name': 'aaa'}), content_type='application/json')
+            self.client.post('/admin/auth/group/add/', u'{ג' + json.dumps({'name': 'aaa'}), content_type='application/json')
             l.check(
-                ('reqdata', 'INFO', '(Anonymous) POST /admin/auth/group/add/\n    {{"name": "aaa"}')
+                ('reqdata', 'INFO', u'(Anonymous) POST /admin/auth/group/add/\n    {ג{"name": "aaa"}')
             )
 
     def test_raw(self):
@@ -39,4 +48,11 @@ class ReqDataTestCase(BaseTestCase):
             self.client.post('/admin/auth/group/add/', 'aaa\nbbb', content_type='text/plain')
             l.check(
                 ('reqdata', 'INFO', '(Anonymous) POST /admin/auth/group/add/\n    aaa\n    bbb')
+            )
+
+    def test_raw_unicode(self):
+        with LogCapture('reqdata') as l:
+            self.client.post('/admin/auth/group/add/', u'אבגד', content_type='text/plain')
+            l.check(
+                ('reqdata', 'INFO', u'(Anonymous) POST /admin/auth/group/add/\n    אבגד')
             )
